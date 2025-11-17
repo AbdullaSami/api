10caae12a18ce96bf6ce11ab39eaaea35caf9ef6
AbdullaSami <alattariabdulla@gmail.com>
2025-11-17 21:11:17 +0200

    edit typo

--- app/Http/Controllers/Api/WalletController.php
+++ app/Http/Controllers/Api/WalletController.php
@@ -42,6 +42,11 @@ class WalletController extends Controller
             return response()->json([
                 'data'=>$data
                 ]);
+        } catch (\Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage()
+            ], 400);
         }
     }
 
