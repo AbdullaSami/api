b428ea9d9b59c5ced294337d6f2da4440c68d4d4
AbdullaSami <alattariabdulla@gmail.com>
2025-11-22 02:33:33 +0200

    test

--- app/Http/Controllers/Api/WalletController.php
+++ app/Http/Controllers/Api/WalletController.php
@@ -65,6 +65,7 @@ class WalletController extends Controller
         $totalTransfer = $member->wallet->transactions()
             ->where('transaction_type', 'send_internal_transfer')
             ->where('status', 'accepted')
+            ->where('receive_member_id', '!=',$member->id)
             ->sum('amount');
 
         // 4 - Deposit
