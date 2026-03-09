48feedda730039509cd78afb8d7b263be05f8539
AbdullaSami <alattariabdulla@gmail.com>
2026-03-09 22:01:02 +0200

    fix: update user seeder email and increase wallet and token balance to 100000

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,25 +16,18 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    // public function run(): void
-    // {
-    //     User::factory(15)->hasMember()->create();
-    // }
-
 
     public function run(): void
     {
         $accounts = [
-            ['first_name' => 'Nova', 'last_name' => 'Group', 'email' => 'ramezdeeb1994@gmail.com'],
+            ['first_name' => 'Nova', 'last_name' => 'Group', 'email' => 'nova_group@thenovagroupco.com'],
         ];
 
         foreach ($accounts as $data) {
-
             $user = User::factory()->create([
                 'username' => $data['first_name'] . '_' . $data['last_name'],
                 ...$data
             ]);
-
             // create hash and save pin for user
             $user->pin()->create([
                 'pin_hash' => Hash::make('1234'),
@@ -42,10 +35,8 @@ class UserSeeder extends Seeder
             $member = $user->member()->create([
                 'rank_id' => 1
             ]);
-
-            $member->wallet()->create(['balance' => 10000]);
-            $member->tokenWallet()->create(['token_balance' => 10000]);
-
+            $member->wallet()->create(['balance' => 100000]);
+            $member->tokenWallet()->create(['token_balance' => 100000]);
             $member->subscription()->create([
                 'package_id'      => Package::inRandomOrder()->first()->id,
                 'expiration_date' => now()->addMonth()
