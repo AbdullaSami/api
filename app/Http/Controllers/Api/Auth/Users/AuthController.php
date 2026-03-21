22293497d1182efb543cd8a266606465a9f3ab40
AbdullaSami <alattariabdulla@gmail.com>
2026-03-21 18:23:13 +0200

    test

--- app/Http/Controllers/Api/Auth/Users/AuthController.php
+++ app/Http/Controllers/Api/Auth/Users/AuthController.php
@@ -309,7 +309,7 @@ class AuthController extends Controller
             if ($request->hasFile('image')) {
                 $imagePath = $request->file('image')->store('image', 'public');
                 $user->image = URL::to(Storage::url($imagePath));
-                $user->update();
+                $user->save();
             }
             return $this->successResponse('profile image updated successfully ', 'user', $user);
         } catch (\Exception $e) {
