5085fab4da7d53c309403e33804cfc2c8e9d3f7f
AbdullaSami <alattariabdulla@gmail.com>
2026-04-07 22:05:48 +0200

    test

--- database/migrations/2026_04_04_160553_create_course_skill.php
+++ database/migrations/2026_04_04_160553_create_course_skill.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('course_skills_talble', function (Blueprint $table) {
+        Schema::create('course_skill', function (Blueprint $table) {
             $table->id();
             $table->foreignId('course_id')->constrained()->onDelete('cascade');
             $table->foreignId('skill_id')->constrained()->onDelete('cascade');
