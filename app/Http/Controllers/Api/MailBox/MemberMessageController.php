642684320234c2117f341f72864df58790162562
AbdullaSami <alattariabdulla@gmail.com>
2026-05-18 00:14:17 +0300

    test

--- app/Http/Controllers/Api/MailBox/MemberMessageController.php
+++ app/Http/Controllers/Api/MailBox/MemberMessageController.php
@@ -101,8 +101,7 @@ class MemberMessageController extends Controller
     {
         $messageRecipient = MemberMessageRecipient::query()
 
-        ->where('message_id', $id)
-            ->orWhere('recipient_id', auth()->id())
+            ->where('recipient_id', auth()->user()->id)
 
             ->with([
                 'message.sender:id,id_code,username,email',
