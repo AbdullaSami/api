867bf16bf1a8a94d5a009d166a0360a587077476
AbdullaSami <alattariabdulla@gmail.com>
2026-06-13 15:11:18 +0300

    test

--- database/migrations/2026_06_13_120729_add_code_column_to_subscription_table.php
+++ database/migrations/2026_06_13_120729_add_code_column_to_subscription_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('subscription', function (Blueprint $table) {
+        Schema::table('subscriptions', function (Blueprint $table) {
             $table->dropColumn('code');
         });
     }
