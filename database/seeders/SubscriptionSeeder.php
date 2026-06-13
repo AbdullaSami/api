f09a14695795bcb2353f4d2d753e001869c7d437
AbdullaSami <alattariabdulla@gmail.com>
2026-06-13 15:18:24 +0300

    test

--- database/seeders/SubscriptionSeeder.php
+++ database/seeders/SubscriptionSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Subscription;
+use Illuminate\Support\Str;
 class SubscriptionSeeder extends Seeder
 {
     /**
@@ -12,6 +13,18 @@ class SubscriptionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Subscription::whereNull('code')->orWhere('code', '')->each(function (Subscription $subscription) {
+            $date = $subscription->subscribed_at
+                ? $subscription->subscribed_at->format('Y-m-d')
+                : now()->format('Y-m-d');
+
+            do {
+                $code = 'SUB-' . $date . '-' . strtoupper(Str::random(4));
+            } while (Subscription::where('code', $code)->exists());
+
+            $subscription->update(['code' => $code]);
+        });
+
+        $this->command->info('Subscription codes generated successfully.');
     }
 }
